0bc898e63bd4a301c85f012fa2172ad9322cc6f9
Santiago <smarcanoweb@gmail.com>
2020-08-16 14:30:31 +0200

    heroku

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-    socket.emit('m', { data: 'user message?' })
+    setInterval(() => {
+        socket.emit('m', { data: 'user message?' })
+    }, 2000)
     console.log('a user connected');
 });
 
-http.listen(3000, () => {
-    console.log('listening on *:3000');
+http.listen(process.env.PORT, () => {
+    console.log(`listening on ${process.env.PORT}`);
 });
