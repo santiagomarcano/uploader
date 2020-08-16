64855fd6538f56f6586e8fd2960f5a05e6aca956
Santiago <smarcanoweb@gmail.com>
2020-08-16 14:38:50 +0200

    'm'

--- index.js
+++ index.js
@@ -2,16 +2,13 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
-const socket = io()
-
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/send', (req, res) => {
     console.log('send')
-    socket.emit('data!', { newData: true })
-
+    io.sockets.emit('data!', { newData: true })
 })
 
 io.on('connection', () => {
