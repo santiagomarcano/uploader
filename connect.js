84f1291706ec1b7f8b05847895f7f1e9dd0b77f9
Santiago <smarcanoweb@gmail.com>
2020-08-16 15:02:32 +0200

    wip

--- connect.js
+++ connect.js
@@ -1,4 +1,6 @@
 // https://socketiotestio.herokuapp.com/
+const http = require('http');
+const fs = require('fs');
 var socket = require('socket.io-client')('https://socketiotestio.herokuapp.com');
 // var socket = require('socket.io-client')('http://localhost:5000');
 
@@ -9,3 +11,13 @@ socket.on('connect', function () {
 socket.on('m', function (data) {
     console.log(data)
 })
+
+socket.on('upload', function (data) {
+    const file = fs.createWriteStream(data.name);
+    const request = http.get('https://socketiotestio.herokuapp.com', function (response) {
+        response.pipe(file);
+    });
+})
+
+
+
--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-var app = require('express')();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+var siofu = require("socketio-file-upload");
+const express = require('express')
+var app = express();
+var io = require('socket.io')(app);
+
+app.use(siofu.router)
+app.use('/uploads', express.static('uploads'))
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
@@ -15,10 +19,13 @@ app.get('/send', (req, res) => {
 io.on('connection', (socket) => {
     // setInterval(() => {
     socket.emit('m', { data: 'Connected!' })
+    var uploader = new siofu();
+    uploader.dir = "/uploads";
+    uploader.listen(socket);
     // }, 2000)
     console.log('a user connected');
 });
 
-http.listen(process.env.PORT, () => {
+app.listen(process.env.PORT, () => {
     console.log(`listening on ${process.env.PORT}`);
 });
