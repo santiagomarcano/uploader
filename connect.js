9fdee98334887d5c69f3d3b853e88c4b217f5c22
Santiago <smarcanoweb@gmail.com>
2020-08-16 14:36:27 +0200

    'heroku'

--- connect.js
+++ connect.js
@@ -1,6 +1,6 @@
 // https://socketiotestio.herokuapp.com/
-// var socket = require('socket.io-client')('https://socketiotestio.herokuapp.com:3000');
-var socket = require('socket.io-client')('http://localhost:5000');
+var socket = require('socket.io-client')('https://socketiotestio.herokuapp.com');
+// var socket = require('socket.io-client')('http://localhost:5000');
 
 socket.on('connect', function () {
     console.log('connected')
--- index.js
+++ index.js
@@ -2,14 +2,22 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
+const socket = io()
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', (socket) => {
-    setInterval(() => {
-        socket.emit('m', { data: 'user message?' })
-    }, 2000)
+app.get('/send', (req, res) => {
+    console.log('send')
+    socket.emit('data!', { newData: true })
+
+})
+
+io.on('connection', () => {
+    // setInterval(() => {
+    socket.emit('m', { data: 'Connected!' })
+    // }, 2000)
     console.log('a user connected');
 });
 
